79258c60adaf18035bb692f025ca37bceb093dd3
Zach Dicklin <zdicklin@gmail.com>
2015-12-15 16:56:13 -0700

    Truncate note item summaries

--- src/components/NoteListItem.jsx
+++ src/components/NoteListItem.jsx
@@ -10,12 +10,22 @@ var NoteListItem = React.createClass({
     e.preventDefault();
     NoteActions.deleteNote(this.props.token, this.props.note.id);
   },
+  truncateSummaryText(text) {
+    var max_summary_len = 256;
+    var summary = text
+    if(text.length > max_summary_len) {
+      summary = summary.slice(0, max_summary_len) + "...";
+    }
+    return summary;
+  },
   render() {
     return(
       <li className='row' onClick={this.handleViewNote}>
         <div className='col-md-11'>
           <h3>{this.props.note.title}</h3>
-          <p>{this.props.note.body}</p>
+          <p>
+            {this.truncateSummaryText(this.props.note.body)}
+          </p>
         </div>
         <div className='col-md-1 trash' onClick={this.handleDeleteNote}>
           <span><i className="fa fa-trash"></i></span>
